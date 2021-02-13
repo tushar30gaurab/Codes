4c858fb58cec5d1e6e38086a6735e8693437fd4e
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-02-13 21:22:50 +0530

    Equal sum partition (memoized)

---
+++ Algorithms/Dynamic Programming/Equal Sum Partition/equalSumParttion.java
@@ -0,0 +1,68 @@
+import java.util.*;
+public class equalSumParttion {
+
+    static boolean solve()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        boolean flag = false;
+
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum = sum+arr[i];
+        }
+
+        boolean t[][] = new boolean[n+1][sum+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                // if(i==0)
+                //     t[i][j]=true;
+                // if(j==0)
+                //     t[i][j]=false;
+
+            }
+        }
+        if(sum%2!=0)
+        flag=false;
+
+        if(sum%2==0)
+        flag = solveEqualSum(arr,sum/2,n,t);
+
+        return flag;
+    }
+
+    static boolean solveEqualSum(int arr[], int sum, int n, boolean t[][])
+    {
+        if(sum>0 && n==0)
+        return false;
+
+        if(sum==0)
+        return true;
+
+        if(arr[n-1]<=sum)
+            return t[n][sum] = (solveEqualSum(arr, sum-arr[n-1], n-1, t) || solveEqualSum(arr, sum, n-1, t));
+
+        else if(arr[n-1]>sum)
+            return t[n][sum] = solveEqualSum(arr, sum, n-1, t);
+
+            return t[n][sum];
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Equal Sum Partition possible: "+solve());
+    }
+    
+}
