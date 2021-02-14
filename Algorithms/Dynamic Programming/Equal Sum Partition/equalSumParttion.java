e907ed4187c737d36191dead32ec44262cb8d5ef
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-02-14 12:12:15 +0530

    Update equalSumParttion.java

--- Algorithms/Dynamic Programming/Equal Sum Partition/equalSumParttion.java
+++ Algorithms/Dynamic Programming/Equal Sum Partition/equalSumParttion.java
@@ -23,18 +23,6 @@ public class equalSumParttion {
         }
 
         boolean t[][] = new boolean[n+1][sum+1];
-
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<sum+1;j++)
-            {
-                // if(i==0)
-                //     t[i][j]=true;
-                // if(j==0)
-                //     t[i][j]=false;
-
-            }
-        }
         if(sum%2!=0)
         flag=false;
 
