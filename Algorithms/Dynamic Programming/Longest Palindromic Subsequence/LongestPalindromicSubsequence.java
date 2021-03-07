fce9acaf4c9da68f4f30b5a4a509296572ca074b
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-03-07 11:24:11 +0530

    Tablulation Method

---
+++ Algorithms/Dynamic Programming/Longest Palindromic Subsequence/LongestPalindromicSubsequence.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class LongestPalindromicSubsequence {
+
+    static int solve(StringBuilder sb1, StringBuilder sb2, int n,int m)
+    {
+        int t[][] = new int[n+1][m+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<m+1;j++)
+            {
+                if(i==0 || j==0)
+                    t[i][j] = 0;
+
+                else if(sb1.charAt(i-1)==sb2.charAt(j-1))
+                    t[i][j] = 1+t[i-1][j-1];
+                else
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+            }
+        }
+
+        return t[n][m];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter a String:");
+        String s = sc.nextLine();
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        sb1.append(s);
+        sb2.append(s);
+        sb2.reverse(); // reversed string
+
+        sc.close();
+        System.out.println("\nCount of Longest Palindromic Subsequence : "+solve(sb1,sb2,sb1.length(),sb2.length()));
+    }
+}
