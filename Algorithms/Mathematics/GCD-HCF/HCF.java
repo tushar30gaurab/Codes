21ea02974427e0ab8b9b0cd7ab20edd9b04b051d
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-03-12 12:37:23 +0530

    Add files via upload

---
+++ Algorithms/Mathematics/GCD-HCF/HCF.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class HCF {
+
+    static int hcf(int a, int b)
+    {
+        if(b==0)
+            return a;
+        else{
+            //b=a;
+            return hcf(b,a%b);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a and b:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(hcf(a, b));
+        hcf(a,b);
+    }
+}
